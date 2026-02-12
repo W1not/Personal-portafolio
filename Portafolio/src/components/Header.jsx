3b67eff2690b444309514e583a3eed0db361ca0d
W1not <r.raul.rodriguez.7@gmail.com>
2026-02-12 17:59:20 -0600

    Added navbar

---
+++ Portafolio/src/components/Header.jsx
@@ -0,0 +1,119 @@
+import { motion, AnimatePresence } from "motion/react"
+import { useState } from "react"
+
+function Header() {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+            {/* Main container */}
+            <div class="max-w-7x1 mx-auto px-4 py-3 flex items-center justify-between text-white">
+                {/* Logo */}
+                <div class="text-xl font-bold">
+                    R
+                </div>
+
+                {/* Navigation buttons for desktop view, disable when the movile device will be activated*/}
+                <nav class="hidden md:flex space-x-10">
+                    <motion.a href="#"
+                        class="hover:text-bright hover:bg-[#016FB9] p-2 rounded-sm"
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.1 }
+                        }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        Inicio
+                    </motion.a>
+                    <motion.a href="#"
+                        class="hover:text-bright hover:bg-[#016FB9] p-2 rounded-sm"
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.1 }
+                        }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        Proyectos
+                    </motion.a>
+                    <motion.a href="#"
+                        class="hover:text-bright hover:bg-[#016FB9] p-2 rounded-sm"
+                        whileHover={{
+                            scale: 1.1,
+                            transition: { duration: 0.1 }
+                        }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        Contacto
+                    </motion.a>
+                </nav>
+
+                {/* Navigation button for reactive enviroment */}
+                <button class="md:hidden text-2xl"
+                    onClick={() => setOpen(!open)}>
+                    ☰
+                </button>
+
+                {/* Menu for movile devices */}
+                <AnimatePresence>
+                    {open && (
+                        <motion.div
+                            class="fixed inset-0 md:hidden bg-[#016FB9]"
+                            initial={{ height: 0, opacity: 0 }}
+                            animate={{ height: "auto", opacity: 1 }}
+                            exit={{ height: 0, opacity: 0 }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            <nav className="flex flex-col p-6 space-y-4 text-lg">
+                                <motion.a href=""
+                                    class="hover:text-bright hover:bg-[#016FB9] p-2"
+                                    whileHover={{
+                                        x: 15,
+                                        transition: { duration: 0.1 }
+                                    }}
+                                    transition={{ duration: 0.2 }}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    X
+                                </motion.a>
+                                <motion.a href="#"
+                                    class="hover:text-bright hover:bg-[#016FB9] p-2"
+                                    whileHover={{
+                                        x: 15,
+                                        transition: { duration: 0.1 }
+                                    }}
+                                    transition={{ duration: 0.2 }}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    Inicio
+                                </motion.a>
+                                <motion.a href="#"
+                                    class="hover:text-bright hover:bg-[#016FB9] p-2"
+                                    whileHover={{
+                                        x: 15,
+                                        transition: { duration: 0.1 }
+                                    }}
+                                    transition={{ duration: 0.2 }}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    Proyectos
+                                </motion.a>
+                                <motion.a href="#"
+                                    class="hover:text-bright hover:bg-[#016FB9] p-2"
+                                    whileHover={{
+                                        x: 15,
+                                        transition: { duration: 0.1 }
+                                    }}
+                                    transition={{ duration: 0.2 }}
+                                    onClick={() => setOpen(false)}
+                                >
+                                    Contacto
+                                </motion.a>
+                            </nav>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </div>
+        </>
+    )
+}
+
+export default Header
--- Portafolio/src/main.jsx
+++ Portafolio/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { motion } from "motion/react"
+
 import './index.css'
+
 import App from './App.jsx'
+import Header from './components/header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header class=''/>
     <App />
   </StrictMode>,
 )
